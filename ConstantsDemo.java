7598102c0a7dce90bca8d82793a240efcadfa414
Noor Nahar <shoyel1516@gmail.com>
2024-10-15 09:54:49 -0400

    Adding ConstantDemo Class

---
+++ ConstantsDemo.java
@@ -0,0 +1,13 @@
+package com.syntexpro.traning;
+
+public class ConstantsDemo {
+
+    public static void main(String[] args) {
+
+       final float PI = 3.14F; // java convention when you used final then varialbe should be uppercase.
+        //pi = 1;  // whn final keyword is used you cannot changed the value. Once you use final no one can change the value.
+
+        System.out.println(PI);
+
+    }
+}
